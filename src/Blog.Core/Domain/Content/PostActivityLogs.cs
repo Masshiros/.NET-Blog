e9034d1a9f7c1be1c81f3793ba792a741ebc3f2f
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-02-27 13:33:13 +0700

    Init(PostActivityLogs: Init PostActivityLogs Entity

---
+++ src/Blog.Core/Domain/Content/PostActivityLogs.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Blog.Core.Domain.Content
+{
+    [Table("PostActivityLogs")]
+    public class PostActivityLogs
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid BlogId { get; set; }
+
+        public PostStatus FromPostStatus { get; set; }
+        public PostStatus ToStatus { get; set; }
+        public DateTime DateCreated { get; set; }
+        [MaxLength(500)]
+        public string? Note { get; set; }
+        public Guid UsertId { get; set; }
+        
+    }
+}
