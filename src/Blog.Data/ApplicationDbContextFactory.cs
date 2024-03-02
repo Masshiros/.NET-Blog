a38449446ffecb570850cb513956ec2b7a221d28
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-03-02 10:56:01 +0700

    FEAT(Database): Update DbContext Factory + Migration

---
+++ src/Blog.Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Blog.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+           var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+           var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+           builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+           return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
