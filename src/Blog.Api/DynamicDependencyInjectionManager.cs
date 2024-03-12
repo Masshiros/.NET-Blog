21f8a977fa5d5b158724ad580c72dd2d117bfaa0
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-03-12 22:35:13 +0700

    CHORE(DI): Separate Dynamic DI Register in other file

---
+++ src/Blog.Api/DynamicDependencyInjectionManager.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Core.SeedWorks;
+using Blog.Data.Repositories;
+
+namespace Blog.Api
+{
+    public static class DynamicDependencyInjectionManager
+    {
+        public static IServiceCollection DynamicRegisterRepositories(this IServiceCollection services)
+        {
+            var concreteServices = typeof(PostRepository).Assembly.GetTypes()
+                .Where(x => x.GetInterfaces().Any(i => i.Name == typeof(IRepository<,>).Name)
+                            && !x.IsAbstract
+                            && x.IsClass
+                            && !x.IsGenericType);
+
+            foreach (var concreteService in concreteServices)
+            {
+                var allInterfaces = concreteService.GetInterfaces();
+
+                var directInterface =
+                    allInterfaces
+                        .Except(allInterfaces
+                            .SelectMany(t => t.GetInterfaces()))
+                        .FirstOrDefault();
+
+                if (directInterface != null)
+                {
+                    services.Add(new ServiceDescriptor(directInterface, concreteService, ServiceLifetime.Scoped));
+                }
+            }
+
+            return services;
+        }
+    }
+}
