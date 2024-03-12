ebc081e15d5413f667526e27b11cbfa97522af7f
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-03-12 11:07:02 +0700

    FEAT(Core): Config Pagination

---
+++ src/Blog.Core/Models/PageResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Models
+{
+    public class PageResult<T> : BasePageResult where T : class
+    {
+        public List<T> Results { get; set; }
+
+        public PageResult()
+        {
+            Results = new List<T>();
+        }
+
+    }
+}
