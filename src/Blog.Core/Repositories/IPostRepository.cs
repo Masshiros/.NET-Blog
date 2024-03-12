b0e99ce9b8ba13e2c6c34f4a5ec49a1a702c53cf
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-03-12 11:07:40 +0700

    FEAT(Post): Apply pagination for Post

--- src/Blog.Core/Repositories/IPostRepository.cs
+++ src/Blog.Core/Repositories/IPostRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Blog.Core.Domain.Content;
+using Blog.Core.Models;
 using Blog.Core.SeedWorks;
 
 namespace Blog.Core.Repositories
@@ -11,5 +12,6 @@ namespace Blog.Core.Repositories
     public interface IPostRepository : IRepository<Post,Guid>
     {
         Task<List<Post>> GetPopularPostsAsync(int count);
+        Task<PageResult<Post>> GetPostsPagingAsync(string keyword, Guid? categoryId, int pageIndex = 1, int pageSize = 10);
     }
 }
--- src/Blog.Data/Repositories/PostRepository.cs
+++ src/Blog.Data/Repositories/PostRepository.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Blog.Core.Domain.Content;
+using Blog.Core.Models;
 using Blog.Core.Repositories;
 using Blog.Data.SeedWorks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data.Repositories
 {
@@ -20,6 +18,30 @@ namespace Blog.Data.Repositories
         public Task<List<Post>> GetPopularPostsAsync(int count)
         {
             return _context.Posts.OrderByDescending(x => x.ViewCount).Take(count).ToListAsync();
+        }
+
+        public async Task<PageResult<Post>> GetPostsPagingAsync(string keyword, Guid? categoryId, int pageIndex = 1, int pageSize = 10)
+        {
+            var query = _context.Posts.AsQueryable();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                query = query.Where(x => x.Name.Contains(keyword));
+            }
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(x => x.CategoryId == categoryId);
+            }
+
+            var totalRow = await query.CountAsync();
+            query = query.OrderByDescending(x => x.DateCreated).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            return new PageResult<Post>
+            {
+                Results = await query.ToListAsync(),
+                CurrentPage = pageIndex,
+                RowCount = totalRow,
+                PageSize = pageSize
+            }
         }
     }
 }
